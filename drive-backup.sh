81957633be3ebb39b16e7db0ffc23904427dfa8e
Nick <nmapsy@gmail.com>
2014-05-18 21:03:40 -0400

    drive-backup.sh: allow files, not just directories

--- drive-backup.sh
+++ drive-backup.sh
@@ -2,7 +2,18 @@
 set -ue
 
 # N.B.: These path names cannot contain spaces!
-DIRECTORIES="misc/DVDs misc/isos misc/offsite-backup misc/oldversions misc/podcasts misc/ROFLCon"
+PATHS="
+misc/DVDs
+misc/isos
+misc/offsite-backup
+misc/oldversions
+misc/podcasts
+misc/ROFLCon
+misc/DesertBusHomestretch.m4v
+misc/gmail-backup-theqwerty0-2013-03-03.dat
+misc/lrrcraft_old_creative.tgz
+misc/Redneck-Rampage-GOG.exe
+backup.sh"
 
 BACKUP_DIR="2TB-backup"
 SOURCE_ROOT='/media/truecrypt4'
@@ -11,8 +22,7 @@ SOURCE_SIZE=1953511744 # bytes
 DEST_SIZE=488383744    # bytes
 
 USAGE="Usage: \$ $(basename $0)
-This will backup certain directories from the 2TB external drive to the 500GB external drive. The drives must be mounted at $SOURCE_ROOT and $DEST_ROOT, respectively. It uses rsync to do the backup, and will not start unless there is enough free space. The directories are:
-$DIRECTORIES"
+This will backup certain files from the 2TB external drive to the 500GB external drive. The drives must be mounted at $SOURCE_ROOT and $DEST_ROOT, respectively. It uses rsync to do the backup, and will not start unless there is enough free space. The files and directories to be backed up are: $PATHS"
 
 function fail {
   echo "$@" >&2
@@ -32,22 +42,22 @@ if [[ $(df | awk '$6 == "'$DEST_ROOT'" {print $2}') != $DEST_SIZE ]]; then
 fi
 
 # Do the right directories exist?
-for dir in $DIRECTORIES; do
-  if [[ ! -d $SOURCE_ROOT/$dir ]]; then
-    fail "Error: directory \"$SOURCE_ROOT/$dir\" not found."
+for path in $PATHS; do
+  if [[ ! -e $SOURCE_ROOT/$path ]]; then
+    fail "Error: backup target \"$SOURCE_ROOT/$path\" not found."
   fi
 done
 if [[ ! -d $DEST_ROOT/$BACKUP_DIR ]]; then
-  fail "Error: destination directory \"$DEST_ROOT/$BACKUP_DIR\" not found."
+  fail "Error: destination backup directory \"$DEST_ROOT/$BACKUP_DIR\" not found."
 fi
 
 # Check disk space
 free_size=$(df | awk '$6 == "'$DEST_ROOT'" {print $4}')
 free=$((1024 * $free_size))
 bytes_todo=0
-# sum up size of source directories
-for dir in $DIRECTORIES; do
-  dir_size=$(du -sb $SOURCE_ROOT/$dir | awk '{print $1}')
+# sum up size of source paths
+for path in $PATHS; do
+  dir_size=$(du -sb $SOURCE_ROOT/$path | awk '{print $1}')
   bytes_todo=$(($bytes_todo + $dir_size))
 done
 # subtract existing size of destination directory
@@ -58,10 +68,17 @@ if [[ $bytes_todo -gt $free ]]; then
 fi
 
 # Do the actual backup
-for dir in $DIRECTORIES; do
-  dest_path=$DEST_ROOT/$BACKUP_DIR/$dir
-  if [[ ! -d $dest_path ]]; then
-    mkdir -p $dest_path
+for path in $PATHS; do
+  # Make sure there's a parent directory at the destination
+  dest_parent=$(dirname $DEST_ROOT/$BACKUP_DIR/$path)
+  if [[ ! -d $dest_parent ]]; then
+    mkdir -p $dest_parent
   fi
-  rsync --delete -zavXA $SOURCE_ROOT/$dir/ $dest_path/
+  if [[ -d $SOURCE_ROOT/$path ]]; then
+    # if it's a directory
+    rsync --delete -zavXA $SOURCE_ROOT/$path/ $DEST_ROOT/$BACKUP_DIR/$path/
+  else
+    # if it's a file
+    rsync --delete -zavXA $SOURCE_ROOT/$path $dest_parent/
+  fi
 done
