270a280bd8730f55ab05a5d9c1f2be5bc4e66bd2
me <nmapsy@gmail.com>
2015-04-14 13:46:57 -0400

    indicator.sh: Show how old the last ping is instead of a validity threshold.

--- indicator.sh
+++ indicator.sh
@@ -13,21 +13,20 @@ updisp=$(cat "$DataDir/upstatus.txt")
 # $ping_str: Last ping latency
 logfile=$(grep -E ^logfile "$DataDir/upmonitor.cfg" | sed -E 's/^logfile\s*=\s*//')
 read ping ping_time rest <<< $(tail -n 1 "$logfile")
-# figure out if ping is old
-now=$(date +%s)
-thres=$((now-30))
-# if ping is old, mark it as not applicable
-if [[ $ping_time -lt $thres ]] || [[ $updisp == '[OFFLINE]' ]]; then
-  ping_str='N/A'
-elif [[ $ping == 0 ]] || [[ $ping == 0.0 ]]; then
+# If ping is old, mark it as not applicable.
+if [[ $ping == 0 ]] || [[ $ping == 0.0 ]]; then
   ping_str='DROP'
 else
   ping_str="$ping ms"
 fi
+# How old is the last ping?
+now=$(date +%s)
+age=$((now - ping_time))
+ping_str="$ping_str / ${age}s ago"
+
 # if ping is old, and upmonitor doesn't say it's offline, assume it's frozen
 if [[ $updisp != '[OFFLINE]' ]] && [[ $ping_time -lt $thres ]]; then
   updisp='[STALLED]'
 fi
-
 
 echo "[ $free free ]$updisp[ $ping_str ][ $temp ]"
