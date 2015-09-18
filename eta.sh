5a612c4903be5b0f2d01eaf353eaeffa9a39d4e6
me <nmapsy@gmail.com>
2015-09-18 15:14:03 -0400

    eta.sh: Work for countup (theoretically), formatted display.

--- eta.sh
+++ eta.sh
@@ -3,7 +3,7 @@ if [ x$BASH = x ] || [ ! $BASH_VERSINFO ] || [ $BASH_VERSINFO -lt 4 ]; then
   echo "Error: Must use bash version 4+." >&2
   exit 1
 fi
-set -ue
+set -u
 
 PauseDefault=5
 Usage="Usage: \$ $(basename $0) 'command to check' [goal] [pause]
@@ -11,6 +11,8 @@ If a \"goal\" is not given, it is assumed to be 0.
 \"pause\" is in minutes."
 
 function main {
+
+  # Process arguments
   if [[ $# -lt 1 ]] || [[ $1 == '-h' ]]; then
     fail "$Usage"
   fi
@@ -37,33 +39,56 @@ function main {
   start=$($command)
   start_time=$(date +%s)
   if ! isint "$start"; then
-    fail "Error: output of command '$command' is not an integer."
+    fail "Error: command '$command' failed or did not output an integer."
   fi
   if [[ $start -gt $goal ]]; then
     countdown='true'
   else
     countdown=''
   fi
+  echo "Initial: $start | Goal: $goal | Time: $start_time"
 
-  #TODO: Work for countup too.
   sleep 15
-  while [[ $start -gt $goal ]]; do
+  if [[ $countdown ]]; then
+    togo=$((start-goal))
+  else
+    togo=$((goal-start))
+  fi
+  while [[ $togo -gt 0 ]]; do
     current=$($command)
     current_time=$(date +%s)
     if [[ $current == $start ]]; then
       echo "Still $current. No change yet."
     elif ! isint "$current"; then
-      echo "Error: output of command '$command' is not an integer." >&2
+      echo "Error: command '$command' failed or did not output an integer." >&2
     else
-      per_sec=$(calc "($start-$current)/($current_time-$start_time)")
+      if [[ $countdown ]]; then
+        progress=$((start-current))
+      else
+        progress=$((current-start))
+      fi
+      per_sec=$(calc "$progress/($current_time-$start_time)")
       eta_sec=$(calc "($current-$goal)/$per_sec")
-      eta_min=$(calc $eta_sec/60)
-      eta_hr=$(calc $eta_min/60)
-      eta=$(date -d "now + $eta_sec seconds")
-      echo "Current: $current ETA: ${eta:11:8} (${eta_hr:0:4} hours / ${eta_min:0:5} min / $eta_sec sec)"
+      display $eta_sec $current
     fi
     sleep "$pause"m
   done
+}
+
+function display {
+  read eta_sec current <<< $@
+  # Keep 1 decimal point of precision, except for seconds, where we drop all decimals.
+  eta_sec=$(echo "$eta_sec" | sed -E 's/\..*$//')
+  eta_min=$(calc $eta_sec/60 | sed -E 's/\.([0-9]).*$/.\1/')
+  eta_hr=$(calc $eta_min/60 | sed -E 's/\.([0-9]).*$/.\1/')
+  eta=$(date -d "now + $eta_sec seconds")
+  now=$(date)
+  if [[ ${eta:0:10} == ${now:0:10} ]]; then
+    eta=${eta:11:8}
+  else
+    eta=${eta:0:19}
+  fi
+  echo "Current: $current | ETA: $eta ($eta_hr hours / $eta_min min / $eta_sec sec)"
 }
 
 function calc {
