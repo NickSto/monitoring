e5408e6ea6c1ce097c01c484fb2f7cccde342bd6
me <nmapsy@gmail.com>
2015-10-27 01:34:00 -0400

    templot.sh: Translate timestamps into hours in past, label axes, add -h usage.

--- templot.sh
+++ templot.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 set -ue
-POINTS_DEFAULT=250
-LOG_FILE=$HOME/aa/misc/computerthings/logs/temp.log
-PLOT_SCRIPT=$HOME/aa/code/python/single/scatterplot.py
+PointsDefault=250
+LogFile=$HOME/aa/misc/computerthings/logs/temp.log
+PlotScript=$HOME/aa/code/python/single/scatterplot.py
 
 plot_script=$(which scatterplot.py)
 if [[ $? -gt 0 ]]; then
-  plot_script=$PLOT_SCRIPT
+  plot_script=$PlotScript
 fi
 
 if [[ $# -lt 1 ]]; then
-  points=$POINTS_DEFAULT
+  points=$PointsDefault
+elif [[ $1 == '-h' ]]; then
+  echo "Usage: $(basename $0) [timepoints]
+Default number of timepoints: $PointsDefault" >&2
+  exit 1
 else
   points=$1
 fi
 
-tail -n $points $LOG_FILE | $plot_script -x 4 -y 1
+time=$(date +%s)
+tail -n $points $LogFile \
+  | awk '{print ($4-'$time')/60/60, $1}' \
+  | scatterplot.py -x 1 -y 2 -X 'Hours in past' -Y Celsius
