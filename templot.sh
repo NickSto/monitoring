ae4823e49dbf3c3a25e3b24070f8423faa67d1ce
me <nmapsy@gmail.com>
2015-10-27 01:19:03 -0400

    templot.sh: 2014-05-23

--- templot.sh
+++ templot.sh
@@ -4,10 +4,15 @@ POINTS_DEFAULT=250
 STATE_DIR=$HOME/.local/share/nbsdata
 PLOT_SCRIPT=$HOME/aa/code/python/single/scatterplot.py
 
+plot_script=$(which scatterplot.py)
+if [[ $? -gt 0 ]]; then
+  plot_script=$PLOT_SCRIPT
+fi
+
 if [[ $# -lt 1 ]]; then
   points=$POINTS_DEFAULT
 else
   points=$1
 fi
 
-tail -n $points $STATE_DIR/temp.log | $PLOT_SCRIPT -x 4 -y 1
+tail -n $points $STATE_DIR/temp.log | $plot_script -x 4 -y 1
