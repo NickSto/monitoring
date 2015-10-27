f3b2072ba877e6ba99f3fce33fa5eaf00bf59afa
me <nmapsy@gmail.com>
2015-10-27 01:12:53 -0400

    templot.sh: Current version of R version (last modification 2014-05-17).

--- templot.sh
+++ templot.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 set -ue
 
-TEMPS="/home/me/.mymisc/temp.log"
-DATA="/tmp/templot-a63e7e2.tmp"
-IMAGE="/tmp/rplot-temp-a63e7e2.png"
+TEMPS="/home/me/.local/share/nbsdata/temp.log"
 PTS_DEFAULT=250
+#DATA="/tmp/templot-a63e7e2.tmp"
+#IMAGE="/tmp/rplot-temp-a63e7e2.png"
+datafile=$(mktemp)
+imgfile=$(mktemp)
 
-R_SCRIPT='temps = read.table("'$DATA'");
-png(filename="'$IMAGE'");
+R_SCRIPT='temps = read.table("'$datafile'");
+png(filename="'$imgfile'");
 plot(temps[,4],temps[,1]);
 dev.off()'
 
 pts=${1:-$PTS_DEFAULT}
 echo "Showing $pts timepoints"
 
-tail -n $pts $TEMPS | sed -r 's/[^0-9\t]//g' > $DATA
+tail -n $pts $TEMPS | sed -r 's/[^0-9\t]//g' > $datafile
 
 r -e "$R_SCRIPT"
 
-eog $IMAGE 2>/dev/null &
+(eog $imgfile 2>/dev/null; rm $imgfile; rm $datafile) &
