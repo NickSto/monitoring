f46d938aa2cdffca885eaff169649856c53e1b5f
me <nmapsy@gmail.com>
2015-10-27 01:11:40 -0400

    templot.sh: 2013-02-25

--- templot.sh
+++ templot.sh
@@ -4,17 +4,15 @@ set -ue
 TEMPS="/home/me/.mymisc/temp.log"
 DATA="/home/me/.mymisc/templot.tmp"
 IMAGE="/tmp/rplot-temp-a63e7e2.png"
-PTS_DEFAULT=500
+PTS_DEFAULT=250
 
 R_SCRIPT='temps = read.table("'$DATA'");
 png(filename="'$IMAGE'");
 plot(temps[,4],temps[,1]);
 dev.off()'
 
-pts=$PTS_DEFAULT
-if [ $# -gt 0 ]; then
-  pts=$1
-fi
+pts=${1:-$PTS_DEFAULT}
+echo "Showing $pts timepoints"
 
 tail -n $pts $TEMPS | sed -r 's/[^0-9\t]//g' > $DATA
 
