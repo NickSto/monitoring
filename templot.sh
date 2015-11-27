8574f2c0acad0bc72693f0ff4091480d3867787e
me <nmapsy@gmail.com>
2015-11-27 00:12:43 -0500

    templot.sh: Wrap main code in main() function.

--- templot.sh
+++ templot.sh
@@ -9,42 +9,48 @@ If not given, will show the last $SecondsDefault seconds ("$(python -c "print $S
 The amount of time can be given by appending \"s\" (seconds), \"m\" (minutes),
 \"h\" (hours), or \"d\" (days) to a number, like \"6h\"."
 
-plot_script=$(which scatterplot.py)
-if [[ $? -gt 0 ]]; then
-  plot_script=$PlotScript
-fi
 
-seconds=
-points=
-if [[ $# -lt 1 ]]; then
-  seconds=$SecondsDefault
-elif [[ $1 == '-h' ]]; then
-  echo "$Usage" >&2
-  exit 1
-elif [[ $1 =~ ^[0-9]+$ ]]; then
-  points=$1
-elif [[ $1 =~ ^[0-9]+[sSmMhHdD]$ ]]; then
-  unit=$(echo $1 | sed -E 's/[0-9]//g')
-  quantity=$(echo $1 | sed -E 's/[^0-9]//g')
-  case $unit in
-    s|S) seconds=$quantity;;
-    m|M) seconds=$(($quantity*60));;
-    h|H) seconds=$(($quantity*60*60));;
-    d|D) seconds=$(($quantity*60*60*24));;
-  esac
-else
-  echo -e "Error: Time format of \"$1\" not recognized.\n" >&2
-  echo "$Usage" >&2
-  exit 1
-fi
+function main {
+  plot_script=$(which scatterplot.py)
+  if [[ $? -gt 0 ]]; then
+    plot_script=$PlotScript
+  fi
 
-now=$(date +%s)
-if [[ $seconds ]]; then
-  cutoff=$((now-seconds))
-  awk "\$4 > $cutoff {print (\$4-$now)/60/60, \$1}" $LogFile \
-    | scatterplot.py -x 1 -y 2 -X 'Hours in past' -Y Celsius
-else
-  tail -n $points $LogFile \
-    | awk '{print ($4-'$now')/60/60, $1}' \
-    | scatterplot.py -x 1 -y 2 -X 'Hours in past' -Y Celsius
-fi
+  seconds=
+  points=
+  if [[ $# -lt 1 ]]; then
+    seconds=$SecondsDefault
+  elif [[ $1 == '-h' ]]; then
+    echo "$Usage" >&2
+    exit 1
+  elif [[ $1 =~ ^[0-9]+$ ]]; then
+    points=$1
+  elif [[ $1 =~ ^[0-9]+[sSmMhHdD]$ ]]; then
+    unit=$(echo $1 | sed -E 's/[0-9]//g')
+    quantity=$(echo $1 | sed -E 's/[^0-9]//g')
+    case $unit in
+      s|S) seconds=$quantity;;
+      m|M) seconds=$(($quantity*60));;
+      h|H) seconds=$(($quantity*60*60));;
+      d|D) seconds=$(($quantity*60*60*24));;
+    esac
+  else
+    echo -e "Error: Time format of \"$1\" not recognized.\n" >&2
+    echo "$Usage" >&2
+    exit 1
+  fi
+
+  now=$(date +%s)
+  if [[ $seconds ]]; then
+    cutoff=$((now-seconds))
+    awk "\$4 > $cutoff {print (\$4-$now)/60/60, \$1}" $LogFile \
+      | scatterplot.py -x 1 -y 2 -X 'Hours in past' -Y Celsius
+  else
+    tail -n $points $LogFile \
+      | awk '{print ($4-'$now')/60/60, $1}' \
+      | scatterplot.py -x 1 -y 2 -X 'Hours in past' -Y Celsius
+  fi
+}
+
+
+main "$@"
