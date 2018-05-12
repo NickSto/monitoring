8956a027b4f8544f03fc1c03492baf2ef6ebc20e
Nick <nmapsy@gmail.com>
2018-05-11 22:50:07 -0700

    totalmem.sh: Allow different methods of matching (exact, basename, prefix).

--- totalmem.sh
+++ totalmem.sh
@@ -5,8 +5,14 @@ if [ "x$BASH" = x ] || [ ! "$BASH_VERSINFO" ] || [ "$BASH_VERSINFO" -lt 4 ]; the
 fi
 set -ue
 
-Usage="Usage: \$ $(basename "$0") [options] cmd_prefix1 [prefix2 [prefix3 [..]]]
--n: A name to call the RAM user in the human-readable output. Defaults to the first prefix.
+Usage="Usage: \$ $(basename "$0") [options] command1 [command2 [command3 [..]]]
+Default: Match the command to the start of the executable as shown in ps (field 11).
+So '/usr/lib/firefox/' would match '/usr/lib/firefox/browser'.
+-e: Match the command to the exact path of the executable as show in ps.
+    So '/usr/lib/firefox/browser' would only match '/usr/lib/firefox/browser'.
+-b: Match the command to the basename of the executable as shown in ps.
+    So 'browser' would match '/usr/lib/firefox/browser'.
+-n: A name to call the RAM user in the human-readable output. Defaults to the first command.
 -t: Output tab-delimited format with 2 columns: the % of total CPU usage
     (all cores), and the RAM usage, in MB.
 -u: Include the unix timestamp as an additional 1st column in the tsv output."
@@ -18,10 +24,14 @@ function main {
 
   tsv=
   name=
+  exact=
+  basename=
   timestamp=
-  while getopts ":n:tuh" opt; do
+  while getopts ":n:ebtuh" opt; do
   case "$opt" in
       n) name="$OPTARG";;
+      e) ending="true";;
+      b) basename="true";;
       t) tsv="true";;
       u) timestamp="true";;
       h) fail "$USAGE";;
@@ -43,9 +53,18 @@ function main {
 
   cpu=0
   mem=0
-  for prefix in "${@:$OPTIND}"; do
-    read cpu mem <<< $(ps aux | awk -v cpu="$cpu" -v mem="$mem" \
-      'substr($11, 1, '${#prefix}') == "'"$prefix"'" {cpu+=$3; mem+=$4} END {print cpu, mem}')
+  for command in "${@:$OPTIND}"; do
+    if [[ "$exact" ]]; then
+      read cpu mem <<< $(ps aux | awk -v cpu="$cpu" -v mem="$mem" \
+        '$11 == "'"$command"'" {cpu+=$3; mem+=$4} END {print cpu, mem}')
+    elif [[ "$basename" ]]; then
+      read cpu mem <<< $(ps aux | awk -v cpu="$cpu" -v mem="$mem" \
+        '{len = split($11, fields, "/"); if (fields[len] == "'"$command"'") {cpu+=$3; mem+=$4}} \
+         END {print cpu, mem}')
+    else
+      read cpu mem <<< $(ps aux | awk -v cpu="$cpu" -v mem="$mem" \
+        'substr($11, 1, '${#command}') == "'"$command"'" {cpu+=$3; mem+=$4} END {print cpu, mem}')
+    fi
   done
 
   if [[ "$tsv" ]]; then
