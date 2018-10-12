df7c273b82a9cc3af6b05b6fcb9014825b10f91c
Nick <nmapsy@gmail.com>
2018-10-12 16:37:13 -0400

    eta.sh: Quote all the variables.

--- eta.sh
+++ eta.sh
@@ -32,19 +32,19 @@ function main {
   done
 
   # get positional arguments
-  narg=$OPTIND
-  while [[ $narg -le $# ]]; do
+  narg="$OPTIND"
+  while [[ "$narg" -le "$#" ]]; do
     arg=${@:$narg:1}
     narg=$((narg+1))
   done
   positionals=$((narg-OPTIND))
-  if [[ $positionals -lt 2 ]]; then
+  if [[ "$positionals" -lt 2 ]]; then
     fail "$Usage"
   fi
-  goal=${@:$OPTIND:1}
+  goal="${@:$OPTIND:1}"
   command="${@:$OPTIND+1:1}"
   args=
-  if [[ $positionals -ge 3 ]]; then
+  if [[ "$positionals" -ge 3 ]]; then
     args="${@:$OPTIND+2}"
   fi
   quoted=
@@ -59,20 +59,20 @@ function main {
   if ! isint "$pause"; then
     fail "Error: -p pause \"$pause\" is not an integer."
   fi
-  if [[ $start ]] && ! isint "$start"; then
+  if [[ "$start" ]] && ! isint "$start"; then
     fail "Error: -s start \"$start\" is not an integer."
   fi
-  if [[ $start_time ]] && ! isint "$start_time"; then
+  if [[ "$start_time" ]] && ! isint "$start_time"; then
     fail "Error: -t time \"$start_time\" is not an integer."
   fi
-  if [[ $field ]] && ! isint "$field"; then
+  if [[ "$field" ]] && ! isint "$field"; then
     fail "Error: -f field \"$field\" is not an integer."
   fi
 
   # Check initial state.
-  if ! ([[ $start ]] && [[ $start_time ]]); then
-    if [[ $field ]]; then
-      start=$(eval $command $args | awk '{print $'$field'}')
+  if ! ([[ "$start" ]] && [[ "$start_time" ]]); then
+    if [[ "$field" ]]; then
+      start=$(eval $command $args | awk "{print \$$field}")
     else
       start=$(eval $command $args)
     fi
@@ -82,7 +82,7 @@ function main {
 $start"
     fi
   fi
-  if [[ $start -gt $goal ]]; then
+  if [[ "$start" -gt "$goal" ]]; then
     countdown='true'
   else
     countdown=''
@@ -90,25 +90,25 @@ $start"
   echo "Initial time: $start_time | Initial count: $start (goal: $goal)"
 
   sleep 15
-  if [[ $countdown ]]; then
+  if [[ "$countdown" ]]; then
     togo=$((start-goal))
   else
     togo=$((goal-start))
   fi
-  while [[ $togo -gt 0 ]]; do
-    if [[ $field ]]; then
-      current=$(eval $command $args | awk '{print $'$field'}')
+  while [[ "$togo" -gt 0 ]]; do
+    if [[ "$field" ]]; then
+      current=$(eval $command $args | awk "{print \$$field}")
     else
       current=$(eval $command $args)
     fi
     current_time=$(date +%s)
-    if [[ $current == $start ]]; then
+    if [[ "$current" == "$start" ]]; then
       echo "Still $current. No change yet."
     elif ! isint "$current"; then
       echo "Error: command '$command $args' failed or did not output an integer. Output:
 $current" >&2
     else
-      if [[ $countdown ]]; then
+      if [[ "$countdown" ]]; then
         progress=$((start-current))
         togo=$((current-goal))
       else
@@ -117,23 +117,23 @@ $current" >&2
       fi
       per_sec=$(calc "$progress/($current_time-$start_time)")
       sec_togo=$(calc "$togo/$per_sec")
-      display $sec_togo $current
+      display "$sec_togo" "$current"
     fi
-    if [[ $togo -gt 0 ]]; then
+    if [[ "$togo" -gt 0 ]]; then
       sleep "$pause"m
     fi
   done
 
   elapsed=$((current_time-start_time))
-  echo "Reached goal! Total time: "$(sec_to_human_time $elapsed)
+  echo "Reached goal! Total time: "$(sec_to_human_time "$elapsed")
 }
 
 function sec_to_human_time {
   local seconds=$1
   # Keep 1 decimal point of precision, except for seconds, where we drop all decimals.
   local sec_togo=$(echo "$seconds" | sed -E 's/\..*$//')
-  local min_togo=$(calc $sec_togo/60 | sed -E 's/\.([0-9]).*$/.\1/')
-  local hr_togo=$(calc $min_togo/60 | sed -E 's/\.([0-9]).*$/.\1/')
+  local min_togo=$(calc "$sec_togo/60" | sed -E 's/\.([0-9]).*$/.\1/')
+  local hr_togo=$(calc "$min_togo/60" | sed -E 's/\.([0-9]).*$/.\1/')
   if [[ $(calc "int($hr_togo)") -gt 1 ]]; then
     echo "$hr_togo hours"
   elif [[ $(calc "int($min_togo)") -gt 1 ]]; then
@@ -146,14 +146,14 @@ function sec_to_human_time {
 function display {
   read sec_togo current <<< $@
   # Keep 1 decimal point of precision, except for seconds, where we drop all decimals.
-  local togo="$(sec_to_human_time $sec_togo)"
+  local togo=$(sec_to_human_time "$sec_togo")
   local eta=$(date -d "now + $sec_togo seconds")
   local now=$(date)
   # Show date if the ETA isn't on the same day.
-  if [[ ${eta:0:10} == ${now:0:10} ]]; then
-    eta=${eta:11:8}
+  if [[ "${eta:0:10}" == "${now:0:10}" ]]; then
+    eta="${eta:11:8}"
   else
-    eta=${eta:0:19}
+    eta="${eta:0:19}"
   fi
   echo "Current: $current | ETA: $eta ($togo)"
 }
@@ -161,7 +161,7 @@ function display {
 function has_spaces {
   with=$(echo -n "$1" | wc -c)
   without=$(echo -n "$1" | tr -d ' ' | wc -c)
-  if [[ $with == $without ]]; then
+  if [[ "$with" == "$without" ]]; then
     return 1
   else
     return 0
