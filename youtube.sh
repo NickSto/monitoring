dd1a660ebcddc0e788d2d8e4543da63cbb94aeaa
Nick <nmapsy@gmail.com>
2019-02-20 14:10:35 -0700

    youtube.sh: Quote all the variables.

--- youtube.sh
+++ youtube.sh
@@ -22,7 +22,7 @@ Options:
 
 function main {
   # Parse arguments.
-  if [[ $# -ge 1 ]] && [[ $1 == '--help' ]]; then
+  if [[ "$#" -ge 1 ]] && [[ "$1" == '--help' ]]; then
     fail "$Usage"
   fi
   get_formats=
@@ -34,16 +34,16 @@ function main {
     case "$opt" in
       F) get_formats=true;;
       n) get_filename=true;;
-      c) convert_to=$OPTARG;;
-      f) quality=$OPTARG;;
+      c) convert_to="$OPTARG";;
+      f) quality="$OPTARG";;
       p) posted="$OPTARG";;
       h) fail "$Usage";;
     esac
   done
   # Get positionals.
-  url=${@:$OPTIND:1}
-  title=${@:$OPTIND+1:1}
-  if [[ $(($#-OPTIND)) -ge 2 ]]; then
+  url="${@:$OPTIND:1}"
+  title="${@:$OPTIND+1:1}"
+  if [[ "$(($#-OPTIND))" -ge 2 ]]; then
     fail "$Usage"
   fi
 
@@ -53,25 +53,25 @@ function main {
     title='%(title)s'
   fi
 
-  if [[ $get_formats ]] || [[ "$url" == '-F' ]] || [[ "$title" == '-F' ]]; then
+  if [[ "$get_formats" ]] || [[ "$url" == '-F' ]] || [[ "$title" == '-F' ]]; then
     youtube-dl "$url" -F
     return
   fi
 
   site=
   for candidate in {youtube,vimeo,facebook,instagram,twitter}.com clips.twitch.tv; do
-    if echo "$url" | grep -qE '^(https?://)?(www\.)?'"$candidate"; then
+    if echo "$url" | grep -qE "^(https?://)?(www\.)?$candidate"; then
       site=$(echo "$candidate" | awk -F . '{print $(NF-1)}')
       break
     fi
   done
-  if ! [[ $site ]]; then
+  if ! [[ "$site" ]]; then
     fail "Error: Invalid url or domain is not youtube.com, vimeo.com, facebook.com, instagram.com, twitter.com, or twitch.tv (in url \"$url\")."
   fi
 
   quality_args=
-  if [[ $quality ]]; then
-    if [[ $site == youtube ]]; then
+  if [[ "$quality" ]]; then
+    if [[ "$site" == youtube ]]; then
       case "$quality" in
         360) quality_args='-f 18';;
         640) quality_args='-f 18';;
@@ -86,15 +86,15 @@ function main {
   fi
 
   conversion_args=
-  if [[ $convert_to ]]; then
+  if [[ "$convert_to" ]]; then
     valid=
-    for conversion in $ValidConversions; do
-      if [[ $convert_to == $conversion ]]; then
+    for conversion in "$ValidConversions"; do
+      if [[ "$convert_to" == "$conversion" ]]; then
         valid=true
         break
       fi
     done
-    if [[ $valid ]]; then
+    if [[ "$valid" ]]; then
       conversion_args="--extract-audio --audio-format $convert_to"
     else
       fail "Error: Invalid conversion target \"$convert_to\"."
@@ -103,9 +103,9 @@ function main {
 
   # Construct the format string (site-specific).
   format=
-  if [[ $convert_to ]]; then
+  if [[ "$convert_to" ]]; then
     format="$title.%(ext)s"
-  elif [[ $site == youtube ]]; then
+  elif [[ "$site" == youtube ]]; then
     format="$title [src %(uploader)s, %(uploader_id)s] [posted %(upload_date)s] [id %(id)s].%(ext)s"
     uploader_id=$(youtube-dl --get-filename -o '%(uploader_id)s' "$url")
     # Only use both uploader and uploader_id if the id is a channel id like "UCZ5C1HBPMEcCA1YGQmqj6Iw"
@@ -113,18 +113,18 @@ function main {
       echo "uploader_id $uploader_id looks like a username, not a channel id. Omitting channel id.." >&2
       format="$title [src %(uploader_id)s] [posted %(upload_date)s] [id %(id)s].%(ext)s"
     fi
-  elif [[ $site == vimeo ]]; then
+  elif [[ "$site" == vimeo ]]; then
     format="$title [src vimeo.com%%2F%(uploader_id)s] [posted %(upload_date)s] [id %(id)s].%(ext)s"
   elif [[ "$site" == twitch ]]; then
     id=$(echo "$url" | sed -E 's#^https?://clips.twitch.tv/([^/?]+)((\?|/).*)?$#\1#')
     format="$title [src twitch.tv%%2F%(creator)s] [posted %(upload_date)s] [id $id].%(ext)s"
-  elif [[ $site == facebook ]]; then
+  elif [[ "$site" == facebook ]]; then
     url_escaped=$(echo "$url" | sed -E -e 's#^((https?://)?www\.)?##' -e 's#^(facebook\.com/[^?]+).*$#\1#' -e 's#/$##')
     url_escaped=$(url_double_escape "$url_escaped")
     format="$title [src $url_escaped] [posted %(upload_date)s].%(ext)s"
-  elif [[ $site == instagram ]] || [[ $site == twitter ]]; then
+  elif [[ "$site" == instagram ]] || [[ "$site" == twitter ]]; then
     upload_date=$(youtube-dl --get-filename -o '%(upload_date)s' "$url")
-    if [[ $upload_date == NA ]]; then
+    if [[ "$upload_date" == NA ]]; then
       if [[ "$posted" ]]; then
         posted_str="[posted $posted] "
       else
@@ -140,7 +140,7 @@ No upload date could be obtained! You might want to put it in yourself:
   fi
 
   # Get the the resulting filename, then exit, if requested.
-  if [[ $get_filename ]]; then
+  if [[ "$get_filename" ]]; then
     youtube-dl --get-filename -o "$format" "$url" $conversion_args $quality_args
     return
   fi
