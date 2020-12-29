3dc3c15c37ce44ac157f78d919aa556e7c75cb1b
Nick <nmapsy@gmail.com>
2020-12-28 19:23:11 -0500

    gnome-bloat.sh: Change interface, input format. - Give paths to the log files themselves, not their directory.   - And change the default to "mem.gnome.tsv" instead of "gnome-bloat.tsv". - Use 3rd column of memory log input, not 4th.

--- gnome-bloat.sh
+++ gnome-bloat.sh
@@ -7,20 +7,29 @@ set -ue
 unset CDPATH
 
 LogDirDefault="$HOME/aa/computer/logs"
-Usage="Usage: \$ $(basename "$0") [log/dir]"
+UptimeDefault="$LogDirDefault/uptime.tsv"
+GnomeLogDefault="$LogDirDefault/mem.gnome.tsv"
+Usage="Usage: \$ $(basename "$0") [mem.gnome.tsv [uptime.tsv]]"
 
 function main {
 
   # Get arguments.
-  log_dir="${@:$OPTIND:1}"
-  if ! [[ "$log_dir" ]]; then
-    log_dir="$LogDirDefault"
+  gnome_log="$GnomeLogDefault"
+  uptime_log="$UptimeDefault"
+  if [[ "$#" -ge 1 ]]; then
+    if [[ "$1" == '-h' ]] || [[ "$1" == '--help' ]] || [[ "$#" -gt 2 ]]; then
+      fail "$Usage"
+    fi
+    gnome_log="$1"
+    if [[ "$#" == 2 ]]; then
+      uptime_log="$2"
+    fi
   fi
 
-  last_restart=$(awk -F '\t' 'END {print $1}' "$log_dir/uptime.tsv")
+  last_restart=$(awk -F '\t' 'END {print $1}' "$uptime_log")
 
-  awk -F '\t' -v OFS='\t' '$1 > '"$last_restart"' {print $1, $4/1024}' "$log_dir/gnome-bloat.tsv" \
-    | scatterplot.py --date -u x -Y MB -T 'GNOME Memory Usage'
+  awk -F '\t' -v OFS='\t' '$1 > '"$last_restart"' {print $1, $3}' "$gnome_log" \
+    | scatterplot.py --grid --date -u x -Y MB -T 'GNOME Memory Usage'
 }
 
 function fail {
