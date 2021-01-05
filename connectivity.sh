bceac0c7596d79b662148dd8f29262d14a51f610
Nick <nmapsy@gmail.com>
2021-01-05 17:31:22 -0500

    connectivity.sh: Rearrange to modularize awk transformations, add -n option. - Put all awk manipulations in separate functions.   - Then compose them as a single pipeline in the main body.

--- connectivity.sh
+++ connectivity.sh
@@ -7,34 +7,37 @@ set -ue
 
 LogFile="$HOME/aa/computer/logs/upmonitor.tsv"
 PlotScript="$HOME/code/python/single/scatterplot.py"
-HoursDefault=24
-Usage="Usage: \$ $(basename "$0") [-i] [hours]
+HoursDefault=12
+Usage="Usage: \$ $(basename "$0") [-l|-i|-n] [-s start_at] [hours]
 Default number of hours to show: $HoursDefault
 -s: Start this many hours ago (default: same as the number of hours to show).
--i: Invert the Y axis, showing 100/latency instead of Log10(latency)."
+-l: Show Y axis as Log10(latency) (default).
+-i: Invert the Y axis, showing 100/latency.
+-n: Show Y axis as raw millisecond values."
 
 function main {
 
   # Get arguments.
-  inverse=
+  transform="log"
   start=
-  while getopts "is:h" opt; do
+  while getopts "lins:h" opt; do
     case "$opt" in
-      i) inverse="true";;
+      i) transform="inverse";;
+      n) transform="normal";;
+      l) transform="log";;
       s) start="$OPTARG";;
       [h?]) fail "$Usage";;
     esac
   done
   hours="${@:$OPTIND:1}"
-
   if ! [[ "$hours" ]]; then
     hours="$HoursDefault"
   fi
-
   if ! [[ "$start" ]]; then
     start="$hours"
   fi
 
+  # Find the plotting script.
   plot_script=$(which scatterplot.py)
   if [[ "$?" -gt 0 ]]; then
     if [[ -x "$PlotScript" ]]; then
@@ -50,6 +53,7 @@ function main {
 
   now=$(date +%s)
 
+  # Figure out sampling rate.
   sampling=$(calc "round($hours/10)")
   if [[ "$sampling" -le 0 ]]; then
     sampling=1
@@ -59,34 +63,71 @@ function main {
   else
     subtitle="every $(ordinal "$sampling") ping"
   fi
+
+  # Figure out start and end of displayed period.
   end=$(calc "$start-$hours")
   start_sec=$(calc "$now - $start*60*60")
   end_sec=$(calc "$now - $end*60*60")
 
-  if [[ "$inverse" ]]; then
-    awk -F '\t' -v OFS='\t' \
-      'NR % '"$sampling"' == 0 && $2 > '"$start_sec"' && $2 < '"$end_sec"' {
-        printf("%f\t", ($2-'"$now"')/60/60)
-        if ($1 == 0) {
-          print 0
-        } else {
-          print 100/$1
-        }
-      }' "$LogFile" \
-      | "$plot_script" --grid --title 'Connectivity' --x-label 'Hours ago' --point-size 5 \
-        --x-range "-$start" "-$end" --y-label 'Connectivity (100/latency)'
+  # Figure out plot labels.
+  case "$transform" in
+    log)
+      title='Latency'$'\n'"($subtitle)"
+      ylabel='Latency (Log10(milliseconds))';;
+    inverse)
+      title='Connectivity'$'\n'"($subtitle)"
+      ylabel='Connectivity (100/latency)';;
+    normal)
+      title='Latency'$'\n'"($subtitle)"
+      ylabel='Latency (milliseconds)';;
+  esac
+
+  # Read log, transform data, and show plot.
+  < "$LogFile" \
+    filter_log "$start_sec" "$end_sec" \
+    | convert_log_times "$now" \
+    | transform_log "$transform" \
+    | "$plot_script" --grid --x-field 1 --y-field 2 --tag-field 3 --title "$title" \
+      --x-range "-$start" "-$end" --x-label 'Hours ago' --point-size 5 --y-label "$ylabel"
+}
+
+function awkt {
+  awk -F '\t' -v OFS='\t' "$@"
+}
+
+function filter_log {
+  awkt -v "start_sec=$1" -v "end_sec=$2" '$2 > start_sec && $2 < end_sec'
+}
+
+function convert_log_times {
+  awkt -v "now=$1" '{
+    $2 = ($2-now)/60/60
+    print
+  }'
+}
+
+function transform_log {
+  local transform="$1"
+  if [[ "$transform" == log ]]; then
+    awkt '{
+      if ($7 == "up" && $1 != 0) {
+        print $2, log($1)/log(10), $7
+      } else {
+        print $2, 0, $7
+      }
+    }'
+  elif [[ "$transform" == inverse ]]; then
+    awkt '{
+      if ($7 == "up" && $1 != 0) {
+        print $2, 100/$1, $7
+      } else {
+        print $2, 0, $7
+      }
+    }'
   else
-    awk -F '\t' -v OFS='\t' \
-      'NR % '"$sampling"' == 0 && $2 > '"$start_sec"' && $2 < '"$end_sec"' {
-        hrs_ago = ($2-'"$now"')/60/60
-        if ($7 == "up" && $1 != 0) {
-          print $7, hrs_ago, log($1)/log(10)
-        } else if ($7 == "down") {
-          print $7, hrs_ago, 0
-        }
-      }' "$LogFile" \
-      | "$plot_script" --grid --tag-field 1 --x-field 2 --y-field 3 --title Latency$'\n'"($subtitle)" \
-        --x-range "-$start" "-$end" --x-label 'Hours ago' --point-size 5 --y-label 'Log10(Latency)'
+    awkt '{
+      print $2, $1, $7
+    }'
   fi
 }
 
